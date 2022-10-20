02809431f966903b6db7f7db2074fd4dafd8b636
git-scott <51733172+git-scott@users.noreply.github.com>
2022-10-20 20:16:08 +1300

    Update txt2imghd.py

--- scripts/txt2imghd.py
+++ scripts/txt2imghd.py
@@ -383,7 +383,7 @@ def realesrgan2x(executable: str, input: str, output: str):
     ])
     process.wait()
     
-    final_output = Image.open("/tmp/outputs/samples/00000u.png/00000u.png")   ### changed from variable named output
+    final_output = Image.open("/tmp/outputs/samples/00000u.png/00000.png")   ### changed from variable named output
     final_output = final_output.resize((int(final_output.size[0] / 2), int(final_output.size[1] / 2)), get_resampling_mode())
     final_output.save(output)
 
